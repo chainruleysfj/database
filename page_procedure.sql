609f09bbce44357fbdf54131a186973207337e54
chainruleysfj <1946800675@qq.com>
2024-06-25 05:50:00 +0800

    删除多余sql

--- page_procedure.sql
+++
@@ -1,9 +0,0 @@
-DROP PROCEDURE IF EXISTS get_movie_production_company;
-DELIMITER //
-CREATE PROCEDURE get_movie_production_company(IN p_movie_id INT)
-BEGIN
-    SELECT production_company_id
-    FROM movie_app_movie
-    WHERE movie_id = p_movie_id;
-END //
-DELIMITER ;
