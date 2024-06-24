7335062962ea364fc2f28516dbacb4e72cebde28
chainruleysfj <1946800675@qq.com>
2024-06-24 20:59:24 +0800

    bug

--- movie_project/movie_app/views.py
+++ movie_project/movie_app/views.py
@@ -488,22 +488,17 @@ def movie_detail(request, movie_id):
             cursor.callproc('get_movie_detail', [movie_id])
             movie_data = cursor.fetchone()
             if  movie_data[7]:
-                print('yes')
                 actors=movie_data[7].split(";")
             else:
-                print('no')
                 actors=None
-            print(8)
             if  movie_data[8]:
                 directors=movie_data[8].split(";")
             else:
                 directors=None
-            print(9)
             if  movie_data[9]:
                 genres=movie_data[9].split(";")
             else:
                 genres=None
-            print(10)
             if  movie_data[10]:
                 narration=movie_data[10].split(";")
             else:
