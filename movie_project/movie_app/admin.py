502ec307eb3825a53bc0c9c89b3766622d25ccef
chainruleysfj <1946800675@qq.com>
2024-06-25 20:40:56 +0800

    收尾调整

--- movie_project/movie_app/admin.py
+++ movie_project/movie_app/admin.py
@@ -1,10 +1,4 @@
 from django.contrib import admin
 from .models import ProductionCompany, Movie, Person, DirectorMovie, MovieGenre, MovieGenreAssociation
 
-admin.site.register(ProductionCompany)
-admin.site.register(Movie)
-admin.site.register(Person)
-admin.site.register(DirectorMovie)
-admin.site.register(MovieGenre)
-admin.site.register(MovieGenreAssociation)
 
